7dc7c4965290f416c6d884254322676caad9ba66
Jaykobzch <jakobboesen11@msn.com>
2013-10-28 18:10:42 +0100

    hej

--- JClicker/Program.cs
+++ JClicker/Program.cs
@@ -17,6 +17,9 @@ namespace JClicker
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new JClicker());
+       
+            //HEJ FRA JAKOB
+        
         }
     }
 }
